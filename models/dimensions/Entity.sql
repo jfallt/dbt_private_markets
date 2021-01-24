09d55271a00f00485c778d3c6d59eb8a94122c98
jfallt <fallt.jared@gmail.com>
2021-01-24 16:51:06 -0500

    check if investable based on VEIC code

--- models/dimensions/Entity.sql
+++ models/dimensions/Entity.sql
@@ -1,42 +1,54 @@
-{{ config(materialized='table') }}
-
-WITH entities_rn as (
+{{ config(materialized = 'table') }}
+WITH entities_rn AS (
 		SELECT [EntityGUID]
-		,[EntityName]
-		,[DomicileCity]
-		,[DomicileStateProvince]
-		,[DomicileCountry]
-		,[PrincipalOperationsCountry]
-		,[UserDescription]
-		,[StockSymbol]
-		,[PublicPrivateStatus]
-		,[IndustryICBIndustry]
-		,[IndustryVEIC10Category]
-		,[IndustryVEIC100Category]
-		,[IndustryVEIC1000Category]
-		,[IndustryVEICCode]
-		,[IndustryVEICName]
-		,[IndustryICBSupersector]
-		,[IndustryICBSector]
-		,[IndustryICBSubsector]
-		,[IndustryICBSubsectorCode]
-		,ROW_NUMBER() OVER (
-			PARTITION BY [EntityGUID] ORDER BY [ReportDateRange] DESC
-			) AS rn
-	FROM [ETL].[CompanyReport]
-	WHERE [EntityGUID] IS NOT NULL
-)
+			,[EntityName]
+			,[DomicileCity]
+			,[DomicileStateProvince]
+			,[DomicileCountry]
+			,[PrincipalOperationsCountry]
+			,[UserDescription]
+			,[StockSymbol]
+			,[PublicPrivateStatus]
+			,CASE 
+				WHEN (
+						IsNull(IndustryVEICCode, - 1) IN (
+							0
+							,300
+							,400
+							,500
+							)
+						)
+					THEN CONVERT(bit, 0)
+				ELSE CONVERT(bit, 1)
+				END AS IsInvestable
+			,[IndustryICBIndustry]
+			,[IndustryVEIC10Category]
+			,[IndustryVEIC100Category]
+			,[IndustryVEIC1000Category]
+			,[IndustryVEICCode]
+			,[IndustryVEICName]
+			,[IndustryICBSupersector]
+			,[IndustryICBSector]
+			,[IndustryICBSubsector]
+			,[IndustryICBSubsectorCode]
+			,ROW_NUMBER() OVER (
+				PARTITION BY [EntityGUID] ORDER BY [ReportDateRange] DESC
+				) AS rn
+		FROM [ETL].[CompanyReport]
+		WHERE [EntityGUID] IS NOT NULL
+		)
 
-SELECT HASHBYTES ( 'MD5', [EntityGUID]) as EntityID
+SELECT HASHBYTES('MD5', [EntityGUID]) AS EntityID
 	,[EntityGUID]
 	,[EntityName]
+	,[PublicPrivateStatus]
+	,[IsInvestable]
 	,[DomicileCity]
 	,[DomicileStateProvince]
 	,[DomicileCountry]
 	,[PrincipalOperationsCountry]
 	,[UserDescription]
 	,[StockSymbol]
-	,[PublicPrivateStatus]
 	,[IndustryVEIC10Category]
 	,[IndustryVEIC100Category]
 	,[IndustryVEIC1000Category]
