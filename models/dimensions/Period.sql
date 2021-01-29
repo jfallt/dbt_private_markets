d049594e446a191dd691b44bd80a88b968fbc61e
jfallt <fallt.jared@gmail.com>
2021-01-29 15:07:19 -0500

    create period dimension table

---
+++ models/dimensions/Period.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='table') }}
+WITH reporting_dates
+AS (
+	SELECT DISTINCT cast([ReportDateRange] AS DATE) AS AsOfDate
+	FROM [ETL].[CompanyReport]
+	)
+SELECT ROW_NUMBER() OVER (
+		ORDER BY AsOfDate
+		) AS PeriodId
+	,AsOfDate
+FROM reporting_dates
