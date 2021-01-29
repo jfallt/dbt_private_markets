d7d21fbde558b8f29a04b557059a6b251bc18233
jfallt <fallt.jared@gmail.com>
2021-01-29 15:14:32 -0500

    create gpfundinvestment table

---
+++ models/dimensions/GPFundInvestment.sql
@@ -0,0 +1,33 @@
+{{ config(materialized = 'table') }}
+WITH entities_rn
+AS (
+	SELECT u.InvestmentAlias
+		,f.GPFundId
+		,e.EntityId
+		,u.[EntityGUID]
+		,u.OrganizationGUID
+		,cast(u.InvestmentDate AS DATE) AS InvestmentDate
+		,cast(u.ExitDate AS DATE) AS ExitDate
+		,u.IsNetOtherAssets
+		,isnull(u.PreMoneyValuationLocal, 0) AS PreMoneyValuationLocal
+		,isnull(u.PreMoneyValuationClient, 0) AS PreMoneyValuationClient
+		,ROW_NUMBER() OVER (
+			PARTITION BY u.OrganizationGUID
+			,u.[EntityGUID] ORDER BY [ReportDateRange] DESC
+			) AS rn
+	FROM [ETL].[CompanyReport] AS u
+	LEFT JOIN dbo.GPFund AS f ON f.OrgGUID = u.OrganizationGUID
+	LEFT JOIN dbo.Entity AS e ON e.EntityGUID = u.EntityGUID
+	WHERE u.[EntityGUID] IS NOT NULL
+	)
+
+SELECT HASHBYTES('MD5', CONCAT(OrganizationGUID,[EntityGUID])) AS GPFundInvestmentId
+	,GPFundId
+	,EntityId
+	,cast(InvestmentDate AS DATE) AS InvestmentDate
+	,cast(ExitDate AS DATE) AS ExitDate
+	,IsNetOtherAssets
+	,PreMoneyValuationLocal
+	,PreMoneyValuationClient
+FROM entities_rn
+WHERE rn = 1
