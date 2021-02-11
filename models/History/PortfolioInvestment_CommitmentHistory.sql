6997af0cc816ea808eac9bd9ab1e6282fac644a3
jfallt <fallt.jared@gmail.com>
2021-02-11 14:05:18 -0500

    move existing to stg, add effective and sec calcs

--- models/History/PortfolioInvestment_CommitmentHistory.sql
+++ models/History/PortfolioInvestment_CommitmentHistory.sql
@@ -9,86 +9,30 @@
     }) 
 }}
 
--- =============================================
--- Description:	Convert staged commitment history
--- into effective commitment & adjusted commitment
--- by quarter
--- =============================================
-WITH CommitByQuarter -- convert the staged commitment history into quarterly data
-AS (
-	SELECT ph.PortfolioInvestmentId
-		,ph.[PortfolioId]
-		,ph.GPFundId
-		,pd.asofdate
-		,ph.EffectiveDate
-		,ph.AdjustedCommitmentAmountLocal
-		,ph.CommitmentAmountLocal
-		,ROW_NUMBER() OVER (
-			PARTITION BY ph.PortfolioId
-			,ph.GPFundId
-			,AsOfDate ORDER BY [EffectiveDate] DESC
-			) AS rn
-	FROM Period pd
-	CROSS APPLY (
-		SELECT *
-		FROM {{ref('stg_PortfolioInvestment_CommitmentHistory')}}
-		) AS ph
-	WHERE ph.EffectiveDate <= pd.[AsOfDate]
-	)
-
-SELECT cbq.AsOfDate
-	,cbq.PortfolioInvestmentId
-	,QFFIFunding.IsFunded
-	,QFFIFunding.EffectiveAdjustedCommitmentAmountClient
-	,QFFIFunding.EffectiveAdjustedCommitmentAmountLocal
-	,QFFIFunding.EffectiveCommitmentAmountClient
-	,QFFIFunding.EffectiveCommitmentAmountLocal
-FROM CommitByQuarter cbq
-INNER JOIN {{ref('Portfolio')}} p ON p.PortfolioID = cbq.PortfolioId
-INNER JOIN {{ref('GPFund')}} gpf ON gpf.GPFundId = cbq.GPFundId
-CROSS APPLY (
-	SELECT dbo.fnFXRate(gpf.LocalCurrencyCode, p.LocalCurrencyCode, cbq.AsOfDate) AS FxRate
-	) AS ComputedValues
-CROSS APPLY (
-	-- valuation dates
-	SELECT MAX(piv.ValuationDate) AS LatestValuationDate
-	FROM PortfolioInvestment_Valuations AS piv
-	WHERE piv.PortfolioInvestmentId = cbq.PortfolioInvestmentId
-		AND piv.ValuationDate <= cbq.AsOfDate
-	) AS PIvaluationData
-OUTER APPLY dbo.fnPortfolioInvestmentCashFlowData(cbq.PortfolioInvestmentId, PIvaluationData.LatestValuationDate) AS PICFD -- cash flow data
-CROSS APPLY (
-	-- calculated effective and effective adjusted commitmentment
-	SELECT FundingStatus.IsFunded
-		,CASE FundingStatus.IsFunded
-			WHEN 1
-				THEN cbq.CommitmentAmountLocal
-			ELSE 0
-			END AS EffectiveCommitmentAmountLocal
-		,CASE FundingStatus.IsFunded
-			WHEN 1
-				THEN cbq.AdjustedCommitmentAmountLocal
-			ELSE 0
-			END AS EffectiveAdjustedCommitmentAmountLocal
-		,CASE FundingStatus.IsFunded
-			WHEN 0
-				THEN 0
-			ELSE Convert(MONEY, (cbq.CommitmentAmountLocal - ((IsNull(PICFD.TotalFundingLocal, 0) * - 1) - IsNull(PICFD.TotalAmountRecallableLocal, 0))) / ComputedValues.FxRate) + ((IsNull(PICFD.TotalFundingClient, 0) * - 1) - IsNull(PICFD.TotalAmountRecallableClient, 0))
-			END AS EffectiveCommitmentAmountClient
-		,CASE FundingStatus.IsFunded
-			WHEN 0
-				THEN 0
-			ELSE Convert(MONEY, (cbq.AdjustedCommitmentAmountLocal - ((IsNull(PICFD.TotalFundingLocal, 0) * - 1) - IsNull(PICFD.TotalAmountRecallableLocal, 0))) / ComputedValues.FxRate) + ((IsNull(PICFD.TotalFundingClient, 0) * - 1) - IsNull(PICFD.TotalAmountRecallableClient, 0))
-			END AS EffectiveAdjustedCommitmentAmountClient
-	FROM (
-		-- determine funding status based on the first cash flow date
-		SELECT CASE 
-				WHEN PICFD.MinFundingDate IS NULL
-					THEN Convert(BIT, 0)
-				WHEN PICFD.MinFundingDate > cbq.AsOfDate
-					THEN Convert(BIT, 0)
-				ELSE Convert(BIT, 1)
-				END AS IsFunded
-		) AS FundingStatus
-	) AS QFFIFunding
-WHERE rn = 1
+SELECT PICH.PortfolioInvestmentId
+	,PICH.IsFunded
+	,PICH.AsOfDate 
+	,PICH.EffectiveCommitmentAmountLocal
+	,PICH.EffectiveCommitmentAmountClient
+	,PICH.EffectiveAdjustedCommitmentAmountLocal
+	,PICH.EffectiveAdjustedCommitmentAmountClient
+	,PICH.EffectiveCommitmentAmountLocal - IsNull(prevPICH.EffectiveCommitmentAmountLocal, 0) AS EffectiveCommitedAmountLocal
+	,PICH.EffectiveCommitmentAmountClient - ISNULL(prevPICH.EffectiveCommitmentAmountClient, 0) AS EffectiveCommitedAmountClient
+	,CASE (poi.[Secondary])
+		WHEN N'True'
+			THEN PICH.EffectiveAdjustedCommitmentAmountLocal - IsNull(prevPICH.EffectiveAdjustedCommitmentAmountLocal, 0)
+		ELSE PICH.EffectiveCommitmentAmountLocal - IsNull(prevPICH.EffectiveCommitmentAmountLocal, 0)
+		END AS [SecondaryCommitedAmountLocal]
+	,CASE (poi.[Secondary])
+		WHEN N'True'
+			THEN PICH.EffectiveAdjustedCommitmentAmountClient - IsNull(prevPICH.EffectiveAdjustedCommitmentAmountClient, 0)
+		ELSE PICH.EffectiveCommitmentAmountClient - IsNull(prevPICH.EffectiveCommitmentAmountClient, 0)
+		END AS [SecondaryCommitedAmountClient]
+FROM {{ref('stg_PortfolioInvestment_CommitmentHistory')}} PICH
+INNER JOIN {{ref('PortfolioInvestment')}} poi on poi.PortfolioInvestmentID = pich.PortfolioInvestmentId
+OUTER APPLY (
+	SELECT *
+	FROM {{ref('stg_PortfolioInvestment_CommitmentHistory')}} PICH2
+	WHERE pich2.PortfolioInvestmentId = pich.PortfolioInvestmentId
+		AND [dbo].[fnPreviousQuarterEndDate](pich.AsOfDate) = pich2.AsOfDate
+	) prevPICH
